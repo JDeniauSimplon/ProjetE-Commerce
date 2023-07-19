5d91bbf361783e651396d699ac75b3049d55b712
Toufik <said.toufik.simplon@gmail.com>
2023-07-19 16:47:12 +0200

    fetch category and products from api

--- frontend/src/app/components/content.component.tsx
+++ frontend/src/app/components/content.component.tsx
@@ -3,32 +3,24 @@ import React, { useState, useEffect } from 'react';
 import styles from './content.component.module.css'
 import Image from 'next/image';
 
-interface Category {
-    id: number;
-    name: string;
-}
-
 interface Product {
-    category_id: number;
+    categoryid: number;
     name: string;
     description: string;
 }
 
-interface Data {
-    categories: Category[];
-    products: Product[];
-}
-
-const data: Data = require('../data/products.json');
-
 export default function Content() {
-    console.log(data);
 
+    const [categories, setCategories] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
     const [checkedCategories, setCheckedCategories] = useState<{ [key: number]: boolean }>({});
 
     useEffect(() => {
+        fetchCategories();
+        fetchProducts()
         let initialChecks: { [key: number]: boolean } = {};
-        data.categories.forEach((category, index) => {
+        categories.forEach((category, index) => {
             initialChecks[category.id] = false;
         });
         setCheckedCategories(initialChecks);
@@ -38,7 +30,36 @@ export default function Content() {
         setCheckedCategories(prevState => ({ ...prevState, [id]: isChecked }));
     };
 
+    const fetchCategories = async () => {
+        try {
+            const response = await fetch('http://localhost:8000/api/categories');
+            const data = await response.json();
+            setCategories(data["hydra:member"]);
+            setLoading(false);
+        } catch (error) {
+            console.error('Failed to fetch categories:', error);
+            setLoading(false);
+        }
+    };
+
+    const fetchProducts = async () => {
+        try {
+            const response = await fetch('http://localhost:8000/api/products');
+            const data = await response.json();
+            setProducts(data["hydra:member"]);
+            setLoading(false);
+        } catch (error) {
+            console.error('Failed to fetch categories:', error);
+            setLoading(false);
+        }
+    };
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
     const colors = ['#F6784C', '#C4D600', '#EAAA00', '#ED8B00', '#84BD00'];
+    console.log(categories);
+    console.log(products)
 
     return (
         <>
@@ -47,7 +68,7 @@ export default function Content() {
                 <div>
                     <div className={styles.categories}>
 
-                        {data.categories?.map((category, index) => (
+                        {categories?.map((category, index) => (
                             <label
                                 key={index}
                                 className={`${styles.categoryButton} ${checkedCategories[category.id] ? styles.checked : ''}`}
@@ -69,10 +90,10 @@ export default function Content() {
                     </div>
 
 
-                    {data.products?.map((product: Product, index: number) => (
+                    {products?.map((product: Product, index: number) => (
                         <div
                             key={index}
-                            className={`${styles.product} ${(!Object.values(checkedCategories).includes(true) || checkedCategories[product.category_id]) ? '' : styles.hidden}`}
+                            className={`${styles.product} ${(!Object.values(checkedCategories).includes(true) || checkedCategories[product.category.id]) ? '' : styles.hidden}`}
                         >
                             <h1>{product.name}</h1>
                             <p>{product.description}</p>
@@ -80,9 +101,6 @@ export default function Content() {
                     ))}
                 </div>
             </>
-
-
-
         </>
     );
 }
