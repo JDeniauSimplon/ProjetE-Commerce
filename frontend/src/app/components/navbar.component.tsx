cc117daf189cbec61c14abea3aebfdc5c6e45a8f
tom <tom.pustel.simplon@gmail.com>
2023-07-20 15:20:29 +0200

    modification du header

--- frontend/src/app/components/navbar.component.tsx
+++ frontend/src/app/components/navbar.component.tsx
@@ -1,55 +1,39 @@
 "use client"
-import { useState } from 'react';
-import { Menubar } from 'primereact/menubar';
-import { Dropdown } from 'primereact/dropdown';
+
 import Image from 'next/image';
+import styles from './navbar.module.css'
+import { useState } from 'react';
+
 
 export default function Navbar() {
-    const [selectedCategory, setSelectedCategory] = useState(null);
-    const categories = [
-        { name: 'Epicerie salée' },
-        { name: 'Epicerie sucrée' },
-        { name: 'Produits frais' },
-        { name: 'Boisson' },
-        { name: 'Animalerie' }
-    ];
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-    let items = [
-        {
-            template: () => (
-                <Dropdown
-                    value={selectedCategory}
-                    onChange={(e) => setSelectedCategory(e.value)}
-                    options={categories}
-                    optionLabel="name"
-                    placeholder="All categories"
-                    className="w-full md:w-14rem"
-                />
-            ),
-        },
-        {
-            label: 'Home',
-            icon: 'pi pi-fw pi-home',
-        },
-        {
-            label: 'Explore',
-            icon: 'pi pi-fw pi-search',
-        },
-        {
-            label: 'Help',
-            icon: 'pi pi-fw pi-question',
-        },
-    ];
-
-    const start = (
-        <Image src={"/logo.jpg"} alt="Logo" width={100} height={100} className="mr-2" />
-    );
+    const toggleMobileMenu = () => {
+      setMobileMenuOpen(!isMobileMenuOpen);
+    };
 
     return (
-        <Menubar
-            className="w-full flex justify-between h-5rem"
-            model={items}
-            start={start}
-        />
+        <div className={styles.header}>
+            <nav className={styles.nav}>
+              <Image src={"/logo.jpg"} alt="Logo" width={100} height={100} />
+              <button className={styles.menuButton} onClick={toggleMobileMenu}>
+                ☰
+              </button>
+              <div className={`${styles.right} ${isMobileMenuOpen ? styles.open : ''}`}>
+                <button className={styles.closeButton} onClick={toggleMobileMenu}>X</button>
+                <select className={styles.dropdown}>
+                  <option value="all" className={styles.option}>Toutes les catégories</option>
+                  <option value="salty" className={styles.option}>Epicerie salée</option>
+                  <option value="sweet" className={styles.option}>Epicerie sucrée</option>
+                  <option value="fresh" className={styles.option}>Produits frais</option>
+                  <option value="drinks" className={styles.option}>Boissons</option>
+                  <option value="animal" className={styles.option}>Animalerie</option>
+                </select>
+                <a className={styles.a} href="#">Home</a>
+                <a className={styles.a} href="#">Explore</a>
+                <a className={styles.a} href="#">Help</a>
+              </div>
+            </nav>
+        </div>
     );
 }
