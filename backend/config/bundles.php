af3b05678b37bcf84d02ae8905874483413a1b95
Toufik <said.toufik.simplon@gmail.com>
2023-07-21 14:11:41 +0200

    uninstall webpack, cleaning backend

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -15,5 +15,4 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
